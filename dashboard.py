02ce3b244420c1401287729386a5a2b3f95330b8
Anne <anne@mini-de-admin.home>
2023-06-27 13:50:22 +0200

    pred=res

--- dashboard.py
+++ dashboard.py
@@ -36,8 +36,8 @@ with tab1:
     
     if predict_btn:
         data = idClient
-        pred = request_prediction(idClient)
-        #pred = res['resultat']   
+        #pred = request_prediction(idClient)
+        pred = res['resultat']   
     
     col1, col2, col3 = st.columns([2, 1, 6])
 
